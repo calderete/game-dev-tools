6f17cd41b9a8d3dd2dd2a34f27f88310f4e625ef
Charles <codprestige10@gmail.com>
2018-11-07 14:03:17 -0500

    added unit tests for type description set

--- test/models/player_item_test.rb
+++ test/models/player_item_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class PlayerItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Instant damage type is set correctly" do
+    name = "Instant damage"
+    PlayerItem.create_item({name: name, damage_type_id: 1})
+    assert_equal PlayerItem.find_by(name: name)['damage_type'], "instant"
+  end
+
+  test "AOE damage type is set correctly" do
+    name = "AOE damage"
+    PlayerItem.create_item({name: name, damage_type_id: 2})
+    assert_equal PlayerItem.find_by(name: name)['damage_type'], "aoe"
+  end
+
+  test "KENITIC element type is set correctly" do
+    name = "Kenitic element"
+    PlayerItem.create_item({name: name, element_type_id: 1})
+    assert_equal PlayerItem.find_by(name: name)['element_type'], "kinetic"
+  end
+
+  test "Melee item type is set correctly" do
+    name = "Melee item"
+    PlayerItem.create_item({name: name, item_type_id: 1})
+    assert_equal PlayerItem.find_by(name: name)['item_type'], "melee"
+  end
+
+  test "Special item type is set correctly" do
+    name = "Special item"
+    PlayerItem.create_item({name: name, item_type_id: 2})
+    assert_equal PlayerItem.find_by(name: name)['item_type'], "special_ability"
+  end
+
+  test "Power item type is set correctly" do
+    name = "Power item"
+    PlayerItem.create_item({name: name, item_type_id: 3})
+    assert_equal PlayerItem.find_by(name: name)['item_type'], "power_ability"
+  end
 end
